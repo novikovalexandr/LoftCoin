b3e5f4a457bf1e4033623aa2a038f54638bf5692
Alex <4werwolf@gmail.com>
2019-04-08 23:11:07 +0300

    +fix AppDatabase

--- app/src/main/java/com/alexandr4/loftcoin/data/db/room/AppDatabase.java
+++ app/src/main/java/com/alexandr4/loftcoin/data/db/room/AppDatabase.java
@@ -1,12 +1,13 @@
 package com.alexandr4.loftcoin.data.db.room;
 
 import com.alexandr4.loftcoin.data.db.model.CoinEntity;
+import com.alexandr4.loftcoin.data.db.model.Transaction;
 import com.alexandr4.loftcoin.data.db.model.Wallet;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {CoinEntity.class, Wallet.class}, version = 3)
+@Database(entities = {CoinEntity.class, Wallet.class, Transaction.class}, version = 3)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract CoinDao coinDao();
 
